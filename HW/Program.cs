804fe26ed5dbcfc852ed87a217e889877c4b5939
Shneyder Natalia Sergeevna <nshneyder@ozon.ru>
2024-03-20 21:04:21 +0300

    error

--- HW/Program.cs
+++ HW/Program.cs
@@ -3,12 +3,10 @@ using HW;
 List<Posting> postings = new List<Posting>
 {
     new Posting("ABC123"),
-    new Posting("67F456"),
     new Posting("DEF456"),
     new Posting("GHI789"),
     new Posting("GH7789"),
     new Posting("G9I789"),
-    new Posting("67F456"),
     new Posting("ABC120"),
     new Posting("67F456"),
     new Posting("EZ7KK1EY"),
@@ -17,4 +15,3 @@ List<Posting> postings = new List<Posting>
 };
 
 PostingDB.Add(postings);
-
--- TestHw/Tests.cs
+++ TestHw/Tests.cs
@@ -4,7 +4,8 @@ namespace TestHW;
 
 public class Tests
 {
-    [Fact] public void PostingCountTest()
+    [Fact] 
+    public void PostingCountTest()
     {
         //Arrange
         List<Posting> postings = new List<Posting>
@@ -26,24 +27,9 @@ public class Tests
         Assert.Equal(postings.Count, PostingDB.Postings.Count);
     }
 
-    [Fact] public void PostingIsUniqTest()
+    [Fact] 
+    public void PostingIsUniqTest()
     {
-        //Arrange
-        List<Posting> postings = new List<Posting>
-        {
-            new Posting("ABC123"),
-            new Posting("DEF456"),
-            new Posting("GHI789"),
-            new Posting("GH7789"),
-            new Posting("G9I789"),
-            new Posting("ABC120"),
-            new Posting("67F456"),
-            new Posting("EZ7KK1EY"),
-            new Posting("88789"),
-            new Posting("SD789"),
-        };
-
-        PostingDB.Add(postings);
         long postingId = 7;
 
         // Act
